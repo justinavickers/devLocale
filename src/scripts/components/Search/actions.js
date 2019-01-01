9fa7f38f6ac48453221477620214c6f2f63e586a
justinavickers <justinav38@gmail.com>
2019-01-01 14:11:17 -0600

    done for now, got save and delete

--- src/scripts/components/Search/actions.js
+++ src/scripts/components/Search/actions.js
@@ -1,11 +1,20 @@
 import eventsList from "./eventList";
 import eventsAPI from "./fetchEvent";
+import eventListening from "./eventListeners"
 
 async function searchEvents() {
   const searchBox = document.getElementById("searchBox")
   const response = await eventsAPI.getAllEvents(searchBox.value)
   const eventsFromSearchingDiv = document.getElementById("eventsFromSearching");
   eventsList(response.events, eventsFromSearchingDiv);
+
+  response.events.forEach((event) => {
+    const saveButton = document.getElementById("saveButton" + event.id)
+    saveButton.addEventListener("click", async () => {
+      console.log("SENDING", event)
+      await eventListening.saveButton(event)
+    })
+  })
 }
 
 export function establishActions() {
--- src/scripts/components/Search/eventListeners.js
+++ src/scripts/components/Search/eventListeners.js
@@ -8,23 +8,37 @@ const eventListening = {
     })
   },
 
-  saveButton() {
-    document.addEventListener("click", function (event) {
-        console.log("hi2")
-      if (event.target.textContent === "Save") {
-        console.log("hi")
-        const parentId = event.target.parentNode.id
-        const userId = sessionStorage.getItem("userId")
-        const eventTitle = event.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
-          let savedEvent = {
-            eventId: parentId,
-            userId: userId,
-            eventName: eventTitle
-          }
-        eventsAPI.postFunction("events", savedEvent).then(response => console.log("posted", response))
-      }
+   saveButton(event) {
+    // document.addEventListener("click", function (event) {
+    //     console.log("hi2")
+    //   if (event.target.textContent === "Save") {
+    //     console.log("hi")
+    //     const parentId = event.target.parentNode.id
+    //     const userId = sessionStorage.getItem("userId")
+    //     const eventTitle = event.target.previousElementSibling.previousElementSibling.previousElementSibling
+    //       let savedEvent = {
+    //         eventId: parentId,
+    //         userId: userId,
+    //         eventName: eventTitle
+    //       }
+    //     eventsAPI.postFunction("events", savedEvent).then(response => console.log("posted", response))
+    //   }
 
-    })
+    // })
+
+    const user_id = sessionStorage.getItem("userId")
+    return fetch("http://localhost:3000/createdevents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        event_id: event.id,
+        venue_id: event.venue_id,
+        user_id,
+      })
+
+    });
   }
 }
 export default eventListening
